15aa8a72ce01195f58ce8d2f37115952ae0558ea
Tony Trinh <tony19@gmail.com>
2021-06-01 18:04:30 -0500

    remove vuepress artifacts

--- docs/.vuepress/.temp/internal/clientAppSetups.js
+++
@@ -1,7 +0,0 @@
-import clientAppSetup0 from '/Users/nagaraj/gowork/src/github.com/zarb.network/node_modules/@vuepress/theme-default/lib/client/clientAppSetup.js'
-import clientAppSetup1 from '/Users/nagaraj/gowork/src/github.com/zarb.network/node_modules/@vuepress/plugin-nprogress/lib/client/clientAppSetup.js'
-
-export const clientAppSetups = [
-  clientAppSetup0,
-  clientAppSetup1,
-]
--- docs/.vuepress/.temp/pages/guide/run-index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><h1 id="getting-started"><a class="header-anchor" href="#getting-started">#</a> Getting Started</h1>
-<p>Running a full node and becoming a validator in Zarb is easy. In this section, you will find
-information and guides to set up a full node and become a validator in the Zarb network.</p>
-</template>
